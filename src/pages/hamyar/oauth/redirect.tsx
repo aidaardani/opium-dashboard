48cf5106c786ff03c7b5b77a0f9ae6271b7d1889
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-07-25 06:32:54 +0330

    Update redirect.tsx

--- src/pages/hamyar/oauth/redirect.tsx
+++ src/pages/hamyar/oauth/redirect.tsx
@@ -2,9 +2,9 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
-import GlobalContextsProvider from "../../../components/plasmic/opium_dashboard/PlasmicGlobalContextsProvider";
+import GlobalContextsProvider from "../../../../components/plasmic/opium_dashboard/PlasmicGlobalContextsProvider";
 
-import { PlasmicHamyarOauthRedirect } from "../../../components/plasmic/opium_dashboard/PlasmicHamyarOauthRedirect";
+import { PlasmicHamyarOauthRedirect } from "../../../../components/plasmic/opium_dashboard/PlasmicHamyarOauthRedirect";
 import { useRouter } from "next/router";
 
 function HamyarOauthRedirect() {
