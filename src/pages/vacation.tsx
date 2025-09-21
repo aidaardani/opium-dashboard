ca776d37f68d58851eed97bfb244d0033421985f
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2025-09-21 17:21:19 +0330

    Refactor import paths in vacation.tsx for consistency

--- src/pages/vacation.tsx
+++ src/pages/vacation.tsx
@@ -2,9 +2,9 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
-import GlobalContextsProvider from "../../../../components/plasmic/opium_dashboard/PlasmicGlobalContextsProvider";
+import GlobalContextsProvider from "../../components/plasmic/opium_dashboard/PlasmicGlobalContextsProvider";
 
-import { PlasmicVacationPage } from "../../../../components/plasmic/opium_dashboard/PlasmicVacationPage";
+import { PlasmicVacationPage } from "../../components/plasmic/opium_dashboard/PlasmicVacationPage";
 import { useRouter } from "next/router";
 
 function VacationPage() {
